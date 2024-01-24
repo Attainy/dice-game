e227bf4642ea8b59f7fb6c8fa91994b95d301454
Attainy <111291076+Attainy@users.noreply.github.com>
2024-01-24 21:42:51 +0900

    1차 완성
    남은 것
    - 최종 점수, 플레이어 이름 입력받기
    - 함수 내에서 전역 변수 가능한 없애기(인자로 받기)
    - 게임종료 화면 적당한 위치 잡기
    - 주사위 3D로

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-/* 번갈아 가면서 주사위를 던지는 게임입니다. 먼저 누적 점수 50점을 만들면 이깁니다.
-
-주사위를 던졌을 때 숫자가 나오면 1~2는 본인 현재 점수 초기화 후 턴 변경,
-3~6은 현재 점수에 나온 주사위 숫자를 더한다. 
-이후에 홀드 하고 현재 점수를 누적 점수에 더할지, 계속 주사위를 던질지 선택한다. */
-
-/* 플레이어 */
-const leftPlayer = document.querySelector('div.left.player');
-const rightPlayer = document.querySelector('div.right.player');
-
-/* 점수 */
-const leftScoreDiv = document.querySelector("div.left div.score");
-const rightScoreDiv = document.querySelector("div.right div.score");
-const diceDiv = document.querySelector(".dice");
-
-/* 버튼 */
-const btnLeftRoleDice = document.querySelector(".left button.role");
-const btnRightRoleDice = document.querySelector(".right button.role");
-const btnLeftHold = document.querySelector('.left button.hold');
-const btnRightHold = document.querySelector('.right button.hold');
-const btnReset = document.querySelector('#reset');
-
-/* 초기 세팅 */
-let leftScore = 0;
-let rightScore = 0;
-let diceScore;
-let turn = true; // true = left, false = right
-let finalScore = 50; // 게임 종료되는 점수
-leftPlayer.style = "background-color: var(--markColor)";
-btnRightRoleDice.style = "display:none";
-btnLeftHold.style = "display:none";
-btnRightHold.style = "display:none";
+/* 번갈아 가면서 주사위를 던지는 게임. 누적 점수 50점을 먼저 만들면 승.
+주사위 1~2점 : 현재 점수 초기화 후 턴 변경
+주사위 3~6점 : 현재 점수 + 주사위 점수 
+-> Hold : 누적 점수 keep하고 턴 변경 | Role : 주사위 굴리기 */
 
 /* 주사위 모양 바꾸기 */
 function changeDiceShape (diceScore) {
@@ -70,20 +42,9 @@ function turnChange (presentTurn) {
     }
 }
 
-/* 현재 플레이어 표시 */
-function presentPlayerMark () {
-    if (turn) {
-        leftPlayer.style = "background-color: var(--markColor)";
-        rightPlayer.style = "background-color: var(--scoreBoardColor)";
-    } else {
-        rightPlayer.style = "background-color: var(--markColor)";
-        leftPlayer.style = "background-color: var(--scoreBoardColor)";
-    }
-}
-
 /* 점수 초기화 */
-function resetScore() {
-    if (turn) {
+function resetScore(presentTurn) {
+    if (presentTurn) {
         leftScoreDiv.innerText = '00';
         leftScore = 0;
     } else {
@@ -92,70 +53,65 @@ function resetScore() {
     }
 }
 
+
+
 /* 게임 종료 시 */
-function gameOver () {
-    const gameOverPopUp = document.createElement('div');
-    const gameOverH1 = document.createElement('h1');
-    const gameOverH2 = document.createElement('h2');
-    const gameOverButton = document.createElement('a');
-
-    document.body.appendChild(gameOverPopUp);
-    gameOverPopUp.appendChild(gameOverH1);
-    gameOverPopUp.appendChild(gameOverH2);
-    gameOverPopUp.appendChild(gameOverButton);
-    gameOverPopUp.classList = 'game-over';
-
-    gameOverButton.addEventListener('click', handleResetBtn);
-
+function gameOver (winner) {
+    // 게임 종료 화면 띄우기
     gameOverH1.innerText = '게임 종료';
-    gameOverButton.innerText = 'New Game';
-    if (turn) {
+    gameOverRestart.innerText = 'New Game';
+    if (winner) {
         gameOverH2.innerText = 'Winner : Player 1';
     } else {
         gameOverH2.innerText = 'Winner : Player 2';
     }
+
+    gameOverPopUp.classList = 'game-over';
 
     btnLeftRoleDice.removeEventListener('click', handleRoleDice);
     btnRightRoleDice.removeEventListener('click', handleRoleDice);
     btnLeftHold.removeEventListener('click', handleHoldBtn);
     btnRightHold.removeEventListener('click', handleHoldBtn);
 
-    btnReset.style = 'diplay: none';
+    btnReset.style = 'display: none';
+    gameOverRestart.addEventListener('click', handleResetBtn);
+}
+
+/* Hold 버튼 눌렀을 때 이벤트 */
+function handleHoldBtn (presentTurn) {
+    turnChange(!presentTurn);   // Turn 변경
+    btnLeftHold.style = "display:none"; // Hold 버튼 숨기기
+    btnRightHold.style = "display:none"; // Hold 버튼 숨기기
 }
 
 /* 점수 누적 */
-function addScore () {
-    if (turn) {
+function addScore (presentTurn) {
+    // Player 1
+    if (presentTurn) {
         leftScore += diceScore;
         leftScoreDiv.innerText = leftScore.toString().padStart(2, '0');
 
         if (leftScore >= finalScore) {
-            gameOver();
+            gameOver(true);
         }
+    // Player 2
     } else {
         rightScore += diceScore;
         rightScoreDiv.innerText = rightScore.toString().padStart(2, '0');
 
         if (rightScore >= finalScore) {
-            gameOver();
+            gameOver(false);
         }
     }
 };
 
-/* Hold 버튼 눌렀을 때 이벤트 */
-function handleHoldBtn () {
-    turnChange(!turn);   // Turn 변경
-    btnLeftHold.style = "display:none"; // Hold 버튼 숨기기
-    btnRightHold.style = "display:none"; // Hold 버튼 숨기기
-}
-
 /* 주사위 점수별 동작 */
-function roleDiceResult (diceScore) {
+function roleDiceResult (diceScore, presentTurn) {
     // 주사위가 1 또는 2로 나왔을 때 : 점수 초기화 후 턴 변경
     if (diceScore <= 2) {
-        resetScore(); // 점수 초기화
-        turnChange(!turn); // 턴 변경
-        if (turn) {
+        resetScore(presentTurn); // 점수 초기화
+        turnChange(!presentTurn); // 턴 변경
+        if (presentTurn) {
             btnLeftHold.style = "display:none"; // Hold 버튼 숨기기
         } else {
             btnRightHold.style = "display:none"; // Hold 버튼 숨기기
@@ -163,8 +119,8 @@ function roleDiceResult (diceScore) {
 
     // 주사위가 3 이상이 나왔을 때 : 점수 누적. 주사위 계속 굴릴지 상대로 넘길지 선택
     } else { 
-        addScore();
-        if (turn) {
+        addScore(presentTurn);
+        if (presentTurn) {
             btnLeftHold.style = "display: visible;"; // hold 버튼 등장
             btnLeftRoleDice.style = "left: calc(50vw - 22.5rem)";
         } else {
@@ -176,13 +132,13 @@ function roleDiceResult (diceScore) {
 
 
 /* 주사위 굴리는 버튼 눌렀을 때 이벤트 */
-function handleRoleDice () {
+function handleRoleDice (presentTurn) {
     // 주사위 점수
     diceScore = Math.floor((Math.random() * 6) + 1);
     // 주사위 모양 바꾸기
     changeDiceShape(diceScore);
     // 점수별 동작
-    roleDiceResult(diceScore);
+    roleDiceResult(diceScore, presentTurn);
 
     console.log('turn: ', turn, 'left', leftScore, 'right', rightScore);
     console.log('diceScore', diceScore)
@@ -190,7 +146,7 @@ function handleRoleDice () {
 
 /* Reset 버튼 눌렀을 때 이벤트 */
 function handleResetBtn () {
-    turnChange (true)
+    turnChange (true);
 
     // 초기화
     leftScore = 0;
@@ -207,11 +163,52 @@ function handleResetBtn () {
         diceDiv.querySelector(`.pip:nth-child(${idx})`).style = "display: initial";
     };
     diceDiv.classList = 'dice';
+
+    // gameover 창 닫기
+    gameOverPopUp.classList = 'pop-up';
+    gameOverH1.innerText = '';
+    gameOverH2.innerText = '';
+    gameOverRestart.innerText = '';
+
+    btnReset.style = 'diplay: initial';
 }
 
+/* 플레이어 */
+const leftPlayer = document.querySelector('div.left.player');
+const rightPlayer = document.querySelector('div.right.player');
 
-btnLeftRoleDice.addEventListener('click', handleRoleDice);
-btnRightRoleDice.addEventListener('click', handleRoleDice);
-btnLeftHold.addEventListener('click', handleHoldBtn);
-btnRightHold.addEventListener('click', handleHoldBtn);
+/* 점수 */
+const leftScoreDiv = document.querySelector("div.left div.score");
+const rightScoreDiv = document.querySelector("div.right div.score");
+const diceDiv = document.querySelector(".dice");
+
+/* 버튼 */
+const btnLeftRoleDice = document.querySelector(".left button.role");
+const btnRightRoleDice = document.querySelector(".right button.role");
+const btnLeftHold = document.querySelector('.left button.hold');
+const btnRightHold = document.querySelector('.right button.hold');
+const btnReset = document.querySelector('#reset');
+
+/* game over 화면 */
+const gameOverPopUp = document.querySelector('div.pop-up');
+const gameOverH1 = document.querySelector('.pop-up h1');
+const gameOverH2 = document.querySelector('.pop-up h2');
+const gameOverRestart = document.querySelector('.pop-up div');
+
+/* 초기 세팅 */
+let leftScore = 0;
+let rightScore = 0;
+let diceScore;
+let turn = true; // true = left, false = right
+let finalScore = 50; // 게임 종료되는 점수
+leftPlayer.style = "background-color: var(--markColor)";
+btnRightRoleDice.style = "display:none";
+btnLeftHold.style = "display:none";
+btnRightHold.style = "display:none";
+
+
+btnLeftRoleDice.addEventListener('click', () => handleRoleDice(turn));
+btnRightRoleDice.addEventListener('click', () => handleRoleDice(turn));
+btnLeftHold.addEventListener('click', () => handleHoldBtn(turn));
+btnRightHold.addEventListener('click', () => handleHoldBtn(turn));
 btnReset.addEventListener('click', handleResetBtn);
